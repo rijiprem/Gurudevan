7686968b90ed687b459d2d50309164caa02fd06d
rijiprem <rijir.kmr@gmail.com>
2021-01-01 16:11:36 +0530

    created a home page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="css\style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+* {box-sizing: border-box}
+body {font-family: Arial, Helvetica, sans-serif;}
+
+</style>
+<body >
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img2.jpg" class="d-block w-100" alt="...">
+          </div>
+          
+        </div>
+      </div>
+<div class="navbar">
+    
+  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+  <a href="#">Malayalam</a> 
+  <a href="#"> English</a> 
+  <a href="#"> Hindi</a>
+  <div style="padding:10px;margin-top:44px;background-image: url(img3.jpg) ; height:1000px;"></div>
+</div>
+
+</body>
+</html> 
